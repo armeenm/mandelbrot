37fd980c2552256a1d7e41f964806951499932a7
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-05-20 06:36:24 -0500

    Refactor clean() into calc() alongside misc. variables

--- mandelbrot/image.cpp
+++ mandelbrot/image.cpp
@@ -8,65 +8,79 @@
 #include <immintrin.h>
 #include <numeric>
 
-auto constexpr inline uset_1 = IntSet{1U};
-
 Image::Image(Args const& args) noexcept
-    : resolution_{args.resolution}, frame_{args.frame}, maxiter_{args.maxiter} {
-
-  std::iota(pixel_offset_x_.lanes.begin(), pixel_offset_x_.lanes.end(), 0);
-}
-
-auto Image::clean() noexcept -> bool {
-  // Check lane empty status //
-  auto const l2sqnorm = z_.l2sqnorm();
-
-  auto constexpr fset_4 = FloatSet{4.0F};
-  auto const over_4 = l2sqnorm > fset_4;
-  auto const reached_iter_limit = iter_ > uset_maxiter_;
-
-  empty_ |= reached_iter_limit | over_4;
-
-  if (_mm256_movemask_epi8(empty_.vec) != -1)
-    return false;
-
-  // Update picture //
-
-  std::memcpy(pos_, iter_.lanes.cbegin(), sizeof(iter_.lanes));
-  pos_ += iter_.lanes.size();
-
-  // Update current pixel indices //
-
-  auto const x_max_reached = current_.x > uset_x_max_;
-
-  current_.x =
-      (~x_max_reached & (current_.x + uset_lanes_incr_)) | (x_max_reached & pixel_offset_x_);
-
-  current_.y += uset_1 & x_max_reached;
-
-  std::copy(current_.x.lanes.cbegin(), current_.x.lanes.cend(), fset_px_.lanes.begin());
-
-  c_.real = fset_px_ * fset_scaling_.real + fset_frame_lower_.x;
-  c_.imag =
-      FloatSet{static_cast<float>(current_.y.lanes[7])} * fset_scaling_.imag + fset_frame_lower_.y;
-
-  iter_ ^= iter_;
-  empty_ ^= empty_;
-  z_.real ^= z_.real;
-  z_.imag ^= z_.imag;
-
-  return pos_ == data_.get() + pixel_count_;
-}
+    : resolution_{args.resolution}, frame_{args.frame}, maxiter_{args.maxiter} {}
 
 auto Image::calc() noexcept -> void {
-  while (!clean()) {
-    auto const temp = z_.real * z_.real - z_.imag * z_.imag + c_.real;
+  auto constexpr uset_1 = IntSet{1U};
+  auto iter = IntSet{0U};
+  auto empty = IntSet{0U};
+
+  auto pixel_x_offset = IntSet{0U};
+  std::iota(pixel_x_offset.lanes.begin(), pixel_x_offset.lanes.end(), 0);
+
+  auto fset_px = FloatSet{};
+  auto fset_frame_lower = GenCoord<FloatSet>{frame_.lower.x, frame_.lower.y};
+  auto fset_scaling = Complex<FloatSet>{{frame_.width() / float(resolution_.x)},
+                                        {frame_.height() / float(resolution_.y)}};
+
+  auto current = PixelSet{.x = pixel_x_offset, .y = 0U};
+  auto z = Complex<FloatSet>{};
+  auto c = Complex<FloatSet>{fset_frame_lower.x, fset_frame_lower.y};
+
+  auto uset_lanes_incr = IntSet{static_cast<std::uint32_t>(iter.lanes.size())};
+  auto uset_x_max = IntSet{resolution_.x - 9};
+  auto uset_maxiter = IntSet{maxiter_ - 1};
+
+  auto data_pos = data_.get();
+
+  do {
+    auto const temp = z.real * z.real - z.imag * z.imag + c.real;
 
     auto constexpr fset_2 = FloatSet{2.0F};
-    z_.imag = fset_2 * z_.real * z_.imag + c_.imag;
-    z_.real = temp;
+    z.imag = fset_2 * z.real * z.imag + c.imag;
+    z.real = temp;
 
-    iter_ += uset_1 & ~empty_;
-  }
+    iter += uset_1 & ~empty;
+
+    // Check lane empty status //
+    auto const l2sqnorm = z.l2sqnorm();
+
+    auto constexpr fset_4 = FloatSet{4.0F};
+    auto const over_4 = l2sqnorm > fset_4;
+    auto const reached_iter_limit = iter > uset_maxiter;
+
+    empty |= reached_iter_limit | over_4;
+
+    if (_mm256_movemask_epi8(empty.vec) == -1) {
+
+      // Update picture //
+
+      std::memcpy(data_pos, iter.lanes.cbegin(), sizeof(iter.lanes));
+      data_pos += iter.lanes.size();
+
+      // Update current pixel indices //
+
+      auto const x_max_reached = current.x > uset_x_max;
+
+      current.x =
+          (~x_max_reached & (current.x + uset_lanes_incr)) | (x_max_reached & pixel_x_offset);
+
+      current.y += uset_1 & x_max_reached;
+
+      std::copy(current.x.lanes.cbegin(), current.x.lanes.cend(), fset_px.lanes.begin());
+
+      c.real = fset_px * fset_scaling.real + fset_frame_lower.x;
+      c.imag =
+          FloatSet{static_cast<float>(current.y.lanes[7])} * fset_scaling.imag + fset_frame_lower.y;
+
+      iter ^= iter;
+      empty ^= empty;
+      z.real ^= z.real;
+      z.imag ^= z.imag;
+    }
+
+  } while (__builtin_expect(data_pos != data_.get() + pixel_count_, 1));
 }
 
 auto Image::save_pgm(std::string_view filename) const noexcept -> bool {
--- mandelbrot/image.h
+++ mandelbrot/image.h
@@ -45,13 +45,8 @@ public:
   [[nodiscard]] [[gnu::cold]] auto frame() const noexcept { return frame_; }
   [[nodiscard]] [[gnu::cold]] auto resolution() const noexcept { return resolution_; }
   [[nodiscard]] [[gnu::cold]] auto maxiter() const noexcept { return maxiter_; }
-  [[nodiscard]] [[gnu::cold]] auto scaling() const noexcept { return scaling_; }
-  [[nodiscard]] [[gnu::cold]] auto pixel_count() const noexcept { return pixel_count_; }
   [[nodiscard]] [[gnu::cold]] auto data() const noexcept { return data_.get(); }
-  [[nodiscard]] [[gnu::cold]] auto current() const noexcept { return current_; }
-  [[nodiscard]] [[gnu::cold]] auto iter() const noexcept { return iter_; }
 
-  auto clean() noexcept -> bool;
   auto calc() noexcept -> void;
   auto save_pgm(std::string_view filename) const noexcept -> bool;
 
@@ -62,28 +57,7 @@ private:
   Frame frame_;
   std::uint32_t maxiter_;
 
-  Complex<float> scaling_ = {.real = frame_.width() / float(resolution_.x),
-                             .imag = frame_.height() / float(resolution_.y)};
-
   std::uint32_t pixel_count_ = resolution_.x * resolution_.y;
 
   std::unique_ptr<std::uint32_t[]> data_ = std::make_unique<std::uint32_t[]>(pixel_count_);
-  std::uint32_t* pos_ = data_.get();
-
-  PixelSet current_ = {.x = pixel_offset_x_, .y = 0U};
-
-  IntSet<std::uint32_t> iter_;
-  IntSet<std::uint32_t> empty_ = IntSet{0U};
-  Complex<FloatSet> z_;
-
-  FloatSet fset_px_;
-
-  IntSet<std::uint32_t> uset_x_max_ = IntSet{resolution_.x - 9};
-  IntSet<std::uint32_t> uset_maxiter_ = IntSet{maxiter_ - 1};
-  IntSet<std::uint32_t> uset_lanes_incr_ = IntSet{std::uint32_t(iter_.lanes.size())};
-
-  GenCoord<FloatSet> fset_frame_lower_ = {frame_.lower.x, frame_.lower.y};
-
-  Complex<FloatSet> c_ = {fset_frame_lower_.x, fset_frame_lower_.y};
-  Complex<FloatSet> fset_scaling_ = {FloatSet{scaling_.real}, FloatSet{scaling_.imag}};
 };
