2fa7a8691129948ca158ab9ae089d7706ed79c61
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-09-04 12:19:55 -0500

    Simplify math

--- src/image.cpp
+++ src/image.cpp
@@ -13,21 +13,24 @@ Image::Image(Args const& args) noexcept
     : resolution_{args.resolution}, frame_{args.frame}, maxiter_{args.maxiter} {}
 
 auto Image::calc() noexcept -> void {
+  // Setup //
   auto constexpr uset_1 = IntSet{1U};
+
+  auto const frame_lower = GenCoord{frame_.lower.x, frame_.lower.y};
+  auto c = Complex<FloatSet>{frame_lower.x, frame_lower.y};
+
+  auto z = Complex<FloatSet>{};
+  auto zsq = Complex<FloatSet>{};
+  auto px = FloatSet{};
   auto iter = IntSet{0U};
+  auto empty = IntSet{0U};
+  auto data_pos = data_.get();
 
   auto const x_max = IntSet{resolution_.x - 9};
   auto const scaling = Complex<FloatSet>{{frame_.width() / static_cast<float>(resolution_.x)},
                                          {frame_.height() / static_cast<float>(resolution_.y)}};
   auto const lanes_incr = IntSet{static_cast<std::uint32_t>(iter.lanes.size())};
-  auto const frame_lower = GenCoord{frame_.lower.x, frame_.lower.y};
   auto const uset_maxiter = IntSet{maxiter_ - 1};
-
-  auto z = Complex<FloatSet>{};
-  auto c = Complex<FloatSet>{frame_lower.x, frame_lower.y};
-  auto px = FloatSet{};
-  auto empty = IntSet{0U};
-  auto data_pos = data_.get();
 
   auto const px_x_offset = []() {
     auto ret = IntSet{0U};
@@ -37,22 +40,23 @@ auto Image::calc() noexcept -> void {
 
   auto current = PixelSet{.x = px_x_offset, .y = 0U};
 
+  // Loop //
   do {
-    auto const temp = z.real * z.real - z.imag * z.imag + c.real;
-
-    auto constexpr fset_2 = FloatSet{2.0F};
-    z.imag = fset_2 * z.real * z.imag + c.imag;
-    z.real = temp;
+    z.imag = (z.real + z.real) * z.imag + c.imag;
+    z.real = zsq.real - zsq.imag + c.real;
+    zsq.real = z.real * z.real;
+    zsq.imag = z.imag * z.imag;
 
     iter += uset_1 & ~empty;
 
     // Check lane empty status //
     auto constexpr fset_4 = FloatSet{4.0F};
-    auto const over_4 = z.l2sqnorm() > fset_4;
+    auto const over_4 = (zsq.real + zsq.imag) > fset_4;
     auto const reached_iter_limit = iter > uset_maxiter;
 
     empty |= reached_iter_limit | over_4;
 
+    // Check if all current pixels are empty //
     if (_mm256_movemask_epi8(empty.vec) == -1) {
       // Update picture //
       std::memcpy(data_pos, iter.lanes.cbegin(), sizeof(iter.lanes));
@@ -75,6 +79,8 @@ auto Image::calc() noexcept -> void {
       empty ^= empty;
       z.real ^= z.real;
       z.imag ^= z.imag;
+      zsq.real ^= zsq.real;
+      zsq.imag ^= zsq.imag;
     }
 
   } while (__builtin_expect(data_pos != data_.get() + pixel_count_, 1));
