be21b4bbae9eec48ab068acf0d42a9c4c3036025
Armeen Mahdian <mahdianarmeen@gmail.com>
2021-02-03 13:47:10 -0600

    Add imgui to build system

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,16 @@ class Mandelbrot(ConanFile):
     version = '1.0.0'
     settings = 'os', 'compiler', 'build_type', 'arch'
     generators = 'cmake_find_package_multi'
-    requires = ['fmt/7.1.3']
+    requires = ['fmt/7.1.3', 'imgui/1.80', 'glfw/3.3.2', 'glew/2.2.0']
     build_policy = 'missing'
+
+    def imports(self):
+        self.copy('imgui_impl_glfw.cpp', src='./misc/bindings', dst='../bindings')
+        self.copy('imgui_impl_opengl2.cpp', src='./misc/bindings', dst='../bindings')
+        self.copy('imgui_impl_glfw.h', src='./misc/bindings', dst='../bindings')
+        self.copy('imgui_impl_opengl2.h', src='./misc/bindings', dst='../bindings')
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(args=['-G', 'Ninja Multi-Config'])
         cmake.build()
