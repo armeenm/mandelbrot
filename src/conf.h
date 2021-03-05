da0187423cfa489279a8ecd61f9daca7d29e3886
Armeen Mahdian <mahdianarmeen@gmail.com>
2021-03-05 14:14:12 -0600

    Hello world

--- src/conf.h
+++ src/conf.h
@@ -1,3 +1,9 @@
 #pragma once
 
 auto constexpr inline profiling = false;
+
+#ifdef N_DEBUG
+auto constexpr inline is_debug = false;
+#else
+auto constexpr inline is_debug = true;
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,8 @@
-#include <chrono>
 #include <fmt/core.h>
-#include <string>
-#include <string_view>
 
-#include "conf.h"
-#include "image.h"
 #include "util.h"
+#include "conf.h"
 
-auto constexpr inline filename_def = "mandelbrot.pgm";
-
-auto main(i32 const argc, char const* const* const argv) -> int {
-  if constexpr (profiling)
-    Image{{}};
-  else {
-    if (argc == 3 || argc > 4) {
-      fmt::print("Usage: {} FILENAME [XRES YRES]", argv[0]);
-      return -1;
-    }
-
-    auto const filename = (argc > 1) ? argv[1] : filename_def;
-
-    auto constexpr stoi = [](std::string_view str) {
-      return static_cast<n32>(std::stoul(str.data()));
-    };
-
-    auto const args = [=] {
-      if (argc > 2)
-        return Image::Args{.resolution = Image::Coord{.x = stoi(argv[2]), .y = stoi(argv[3])}};
-      else
-        return Image::Args{};
-    }();
-
-    auto const start_comp = std::chrono::high_resolution_clock::now();
-
-    auto img = Image{args};
-
-    auto const end_comp = std::chrono::high_resolution_clock::now();
-
-    img.save_pgm(filename);
-
-    auto const end_save = std::chrono::high_resolution_clock::now();
-
-    fmt::print("Total time: {}ms\n", to_ms(start_comp, end_save));
-    fmt::print("  Computation time: {}ms\n", to_ms(start_comp, end_comp));
-    fmt::print("  Saving time: {}ms\n", to_ms(end_comp, end_save));
-  }
+auto main() -> int {
+  fmt::print("Hello world!\n");
 }
